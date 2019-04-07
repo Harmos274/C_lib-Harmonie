725548fd9e94d0d10d9646140ace8e139be852da
Lilian VERLHAC <lilian.verlhac@epitech.eu>
2019-04-07 23:39:55 +0200

    add my_str_isalpha
    explicit name

---
+++ str_tools/my_str_isalpha.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2018
+** my_str_isalpha
+** File description:
+** my_str_isalpha
+*/
+
+int my_char_isalpha(char c)
+{
+    if ((c >= 'a' && c <= 'z') || (c>= 'A' && c <= 'Z'))
+        return (1);
+    return (0);
+}
+
+int my_str_isalpha(char const *str)
+{
+    int e = 0;
+
+    while (str[e] != '\0') {
+        if (my_char_isalpha(str[e]) == 1)
+            e++;
+        else
+            return (0);
+    }
+    return (1);
+}
